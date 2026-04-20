293ab27afc10ca62e71b43b20050b069ef6fa663
Luna Heyman <luna@toodeluna.net>
2026-04-20 21:21:36 +0200

    fix: add nice views

--- nix/packages/firmware/package.nix
+++ nix/packages/firmware/package.nix
@@ -3,7 +3,7 @@ buildSplitKeyboard {
   name = "firmware";
   src = self;
   board = "nice_nano//zmk";
-  shield = "corne_%PART%";
+  shield = "corne_%PART% nice_view_adapter nice_view";
   zephyrDepsHash = "sha256-yZd+C2k9Kb1TKzXS6rR3/Jcs3UAGeKmq0YwdylODRgs=";
   meta.description = "My corne keyboard configuration";
 }
